7ca64507835ed671eb4233b58c5cece4865011c0
pimentelcf <149383331+pimentelcf@users.noreply.github.com>
2024-06-24 19:28:06 -0300

    Add files via upload

---
+++ Aula16/js/index.js
@@ -0,0 +1,16 @@
+/*function ativarContagem() {
+
+    document.getElementById("tempo").innerHTML = "Começo a contar!";
+    setTimeout(function () {
+        document.getElementById("tempo").innerHTML = "Executou o setTimeout";
+    },  5000);
+
+}
+*/
+
+function ativarContagem() {
+    document.getElementById("tempo").innerHTML = "Começo a contar!";
+    setTimeout(function () {
+        document.getElementById("tempo").innerHTML = "Executou o setTimeout";
+    },  5000);
+}
